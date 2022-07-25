be8b8c7fa50afc1a16e1f6769741be001950908b
zoharkiks <zoharkiks@gmail.com>
2022-07-25 12:14:53 +0530

    Initial commit from Create Next App

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    // Use custom colors
+    // colors: {
+    //   'white': '#ffffff',
+    //   'black':'#000000',
+    // },
+    extend: {
+      // Using custom fonts
+      // fontFamily: {
+      //   worldwide: ["WorldwideHeadline", "cursive"],
+      //   blooming: ["Blooming Elegant Sans", "cursive"],
+      //   poppins: ["Poppins", 'sans-serif']
+      // },
+    },
+  },
+  plugins: [],
+}
