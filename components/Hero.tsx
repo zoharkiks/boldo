ad22235258d7ef94007555456e33eae0c526b72d
zoharkiks <zoharkiks@gmail.com>
2022-07-25 13:54:20 +0530

    worked on hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { images } from "../constants";
+import Ticker from "react-ticker";
 
 const Hero = () => {
   return (
-    <div>I'am a Hero Section</div>
-  )
-}
+    <div className="mt-[54px] ">
+      <div className=" flex justify-between text-white">
+        <div className="flex w-1/2 flex-col justify-center  ">
+          <h1 className="mb-4 font-manrope text-[48px] leading-[72px]">
+            Save time by building fast with Boldo Template{" "}
+          </h1>
+          <h2 className="mb-10  font-opensans leading-7">
+            Funding handshake buyer business-to-business metrics iPad
+            partnership. First mover advantage innovator success deployment
+            non-disclosure.
+          </h2>
+          <div className="flex space-x-6 font-opensans text-xl font-bold">
+            <button className="rounded-[56px] bg-green py-4 px-[56px] text-darkBlue">
+              Buy Template
+            </button>
+            <button className=" rounded-[56px] border-2 py-4 px-[56px]">
+              Explore
+            </button>
+          </div>
+        </div>
+        <img src={images.hero.src} alt="" />
+      </div>
+{/* Will add ticker later */}
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
